35dc7b30f8db534494a4e2753cb526f5c0302e4c
Romeo Corpuz <nexus@Romeos-MBP.home>
2018-11-28 21:24:40 -1000

    pages initial setup

--- ohanadaily/src/app/app.module.ts
+++ ohanadaily/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LogoutComponent } from './pages/logout/logout.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ContentsComponent } from './pages/contents/contents.component';
+import { BuyerComponent } from './pages/buyer/buyer.component';
+import { SellerComponent } from './pages/seller/seller.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { BuyerDashboardComponent } from './pages/buyer-dashboard/buyer-dashboard.component';
+import { SellerDashboardComponent } from './pages/seller-dashboard/seller-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent,
+    ContentsComponent,
+    BuyerComponent,
+    SellerComponent,
+    CheckoutComponent,
+    BuyerDashboardComponent,
+    SellerDashboardComponent
   ],
   imports: [
     BrowserModule,
