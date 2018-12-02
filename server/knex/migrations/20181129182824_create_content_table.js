c793ec0f93938a9d1db80d7afddd0ecd2ec08bc4
CwLauger <cwlauger@gmail.com>
2018-12-01 16:42:39 -1000

    migrated and seeded

--- server/knex/migrations/20181129182824_create_content_table.js
+++ server/knex/migrations/20181129182824_create_content_table.js
@@ -4,16 +4,14 @@ exports.up = function(knex, Promise) {
     table.string('type').notNullable();
     table.integer('user_id').references('id').inTable('users').index();
     table.string('title', 320).notNullable();
-    table.date('description').notNullable();
+    table.string('description', 855).notNullable();
     table.string('location', 95).notNullable();
-    table.boolean('bid').notNullable();
+    table.integer('bid').notNullable();
     table.boolean('status').notNullable();
     table.string('category').notNullable();
     table.integer('file_size').notNullable();
     table.string('resolution').notNullable();
-    table.string('file_location').notNullable();
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.string('file_location', 1000).notNullable();
   })
 }
 
