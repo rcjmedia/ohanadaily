1796538045dc09043ae5313ed9d90d052e5035b6
CwLauger <cwlauger@gmail.com>
2018-12-12 19:21:17 -1000

    about page

---
+++ src/app/about/about.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Observable, of } from ‘rxjs’;
+// import { map, catchError } from ‘rxjs/operators’;
+
+@Injectable()
+export class AboutService {
+  baseUrl: string = 'http://34.222.113.192:8080/api/';
+
+  aboutSection: any[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  getAboutSection() {
+    const url = this.baseUrl + 'about/';
+    return this.httpClient.get(url).toPromise();
+  }
+}
