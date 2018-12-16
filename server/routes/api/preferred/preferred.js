72e8545c0741c9dba6da9cc795cbca6a95ace9db
CwLauger <cwlauger@gmail.com>
2018-12-15 19:23:49 -1000

    3rd times a charm

--- server/routes/api/preferred/preferred.js
+++ server/routes/api/preferred/preferred.js
@@ -17,22 +17,6 @@ router.get('/', (req, res) => {
     });
 });
 
-//get by id
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  PreferredModels.where('id', id)
-    .fetchAll({ withRelated: ['buyer_id', 'seller_id'] })
-    .then(preferredId => {
-      console.log('\nServer: Display By Transaction ID\n', preferredId);
-      res.json(preferredId);
-    })
-    .catch(err => {
-      console.log('err', err);
-      res.json('err');
-    });
-});
-
 //post new preferred seller/buyer
 router.post('/preferred/new', (req, res) => {
   console.log('\nthis is req.body for register', req.body);
@@ -59,5 +43,23 @@ router.post('/preferred/new', (req, res) => {
       res.json('err');
     })
   })
+
+//get by id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  PreferredModels.where('id', id)
+    .fetchAll({ withRelated: ['buyer_id', 'seller_id'] })
+    .then(preferredId => {
+      console.log('\nServer: Display By Transaction ID\n', preferredId);
+      res.json(preferredId);
+    })
+    .catch(err => {
+      console.log('err', err);
+      res.json('err');
+    });
+});
+
+
 
 module.exports = router;
