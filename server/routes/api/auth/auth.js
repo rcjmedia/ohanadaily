f006ef7290014d94238e2c93d071017e1da7663d
Romeo Corpuz <romeocorpuz808@gmail.com>
2018-12-25 13:48:58 -1000

    auth update

--- server/routes/api/auth/auth.js
+++ server/routes/api/auth/auth.js
@@ -29,10 +29,10 @@ router.post('/register', (req, res) => {
         console.log('hash', hash)
         return UserModels
         .forge({
-            email: email,
-            password: hash,
             first_name: first_name,
             last_name: last_name,
+            email: email,
+            password: hash,
             birthdate: birthdate,
             address: address,
             rank: rank,
