1f06f328b95927a6eefc91b3ce7e4cd86e9a941a
Romeo Corpuz <romeocorpuz808@gmail.com>
2018-12-29 02:05:41 -1000

    ec2 staging

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const users = require('./users/users');
 const content = require('./content/content');
 const auth = require('./auth/auth');
+// const google = require('./google/app')
 
+// router.use('/gcloud', google);
 router.use('/auth', auth);
 router.use('/users', users);
 router.use('/content', content);
